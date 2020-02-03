fe3591d7b747e2afe7ab1194b45ac4e4febd33c1
EC2 Default User <ec2-user@ip-172-31-89-78.ec2.internal>
2020-02-03 06:47:32 +0000

    rspec testing for username and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+  before(:all) do 
+    @user = User.new(name:"Maya",email:"maya@gmail.com",password_digest:"12345")
+  end
   context "User validation " do
     it "Must have a user name" do 
       user = User.new(email:"maya@gmail.com").save
@@ -13,6 +16,12 @@ RSpec.describe User, :type => :model do
     it "it should save successfully" do 
       user = User.new(name:"Maya",email:"maya@gmail.com").save
       expect(user).to eq(true)
+    end
+    it "It should give user name" do 
+      expect(@user.name).to eq("Maya")
+    end
+    it "It should give email" do 
+      expect(@user.email).to eq("maya@gmail.com")
     end
   end
 end
