a5df9d67a670cb49eb6eac6a054c5376678486b2
IBUNHABIBU <ibunhabib@gmail.com>
2020-02-10 16:52:47 +0000

    writting tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-      
+     context 'GET #index' do 
+        it 'returns a success response' do 
+            get :index
+            expect(response).to be_success
+        end 
+    end 
+    context 'GET #show' do 
+        it 'returns a success response' do 
+            user = User.create!(name:"Salum",email:"maya@gmail.com")
+            get :show, params: { id: user.to_param }
+            expect(response).to be_success
+        end
+    end
 end
