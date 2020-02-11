6b5fba8806527fcebf4a4703f695acd2c6973215
IBUNHABIBU <ibunhabib@gmail.com>
2020-02-11 13:48:52 +0000

    test pass for user created successfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
     def show 
     end
     def new 
+        @user = User.new
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            flash[:success] = 'User was successful created.'
+            redirect_to @user 
+        else
+            render new
+        end
+    end
+    private 
+    def user_params
+        params.require(:user).permit(:name,:email)
     end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,8 +5,8 @@ RSpec.feature "Users", type: :feature do
       scenario 'should be successful' do 
           visit new_user_path
           within('form') do 
-              fill_in 'name', with: 'Maya'
-              fill_in 'Email ', with: 'maya@gmail.com'
+              fill_in 'Name', with: 'Maya'
+              fill_in 'Email', with: 'maya@gmail.com'
           end
           click_button 'Create User'
           expect(page).to have_content("User was successful created.")
