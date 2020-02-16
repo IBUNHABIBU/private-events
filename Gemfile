f32750a1d67cf115c22920252c17f7b82250cb50
IBUNHABIBU <ibunhabib@gmail.com>
2020-02-16 00:21:42 +0000

    adding db cleaner

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.8'
   gem 'factory_bot_rails'
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,16 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  
+   config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
