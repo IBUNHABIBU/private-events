b53013bae3cd93a1331593ea7dcd707620662d21
maya88en <maya88en@gmail.com>
2020-02-17 20:07:16 +0200

    adding bootstrap

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
+gem 'bootstrap-sass', '3.3.7'
 # Use sqlite3 as the database for Active Record
  gem 'sqlite3'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'signup', to: 'users#new'
     resources :users
