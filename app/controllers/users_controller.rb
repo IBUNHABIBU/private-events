29228d02d8ea3fb5d105ae96bf36c60e55aa1119
IBUNHABIBU <ibunhabib@gmail.com>
2020-02-23 10:33:36 +0000

    showing all user events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  def index 
+    @user = User.all
+  end
   def create
     @user = User.new(user_params)
     if @user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
-  
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'signup', to: 'users#new'
-    resources :users
-    resources :events
+    resources :users 
+    
     get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
