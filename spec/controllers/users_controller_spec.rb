a3f6a99ab69ce526bc034913de29121cd5fc6a40
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-02 15:07:45 +0000

    correcting lint errors

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  context 'GET #new' do
-    it 'returns a success response' do
-      get :new
-      expect(response).to be_success
+    context 'GET #new' do 
+        it 'returns a success response' do 
+            get :new
+            expect(response).to be_success
+        end 
+     end 
+    context 'GET #show' do 
+        it 'returns a success response' do 
+            user = create(:user)
+            get :show
+            expect(response).to be_success
+        end
+        it "renders the #show view" do
+            user = create(:user)
+            get :show, params: { id: user.to_param }
+            response.should render_template :show
+        end
+        it "assigns the requested user to @user" do
+            user = create(:user)
+            get :show, params: { id: user.to_param }
+            assigns(:user).should eq(user)
+        end
     end
-  end
-  context 'GET #show' do
-    it 'returns a success response' do
-      user = create(:user)
-      get :show
-      expect(response).to be_success
+    
+    describe "GET #index" do
+          it "displays all users" do
+            user = create(:user)
+            get :index
+            expect(assigns(:users)).to eq([user])
+          end
+  
+          it "renders the :index view" do
+            get :index
+            expect(response).to render_template("index")
+          end
     end
-    it 'renders the #show view' do
-      user = create(:user)
-      get :show, params: { id: user.to_param }
-      response.should render_template :show
-    end
-    it 'assigns the requested user to @user' do
-      user = create(:user)
-      get :show, params: { id: user.to_param }
-      assigns(:user).should eq(user)
-    end
-  end
-
-  describe 'GET #index' do
-    it 'displays all users' do
-      user = create(:user)
-      get :index
-      expect(assigns(:users)).to eq([user])
-    end
-
-    it 'renders the :index view' do
-      get :index
-      expect(response).to render_template('index')
-    end
-  end
 end
