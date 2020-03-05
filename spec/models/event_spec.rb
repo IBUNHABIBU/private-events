199d9c45983699d76e9d7fc5114cfd7f847b9c35
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-05 16:40:53 +0000

    fixing lint errors

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-   let(:event) { build(:event) }
+  let(:event) { build(:event) }
   context 'Creating Event' do
     it 'should give event title' do
       event.title = 'Sido'
@@ -15,13 +15,11 @@ RSpec.describe Event, type: :model do
       event.location = 'Musoma'
       expect(event.location).to eq('Musoma')
     end
-   
   end
   context 'Event Associations' do
     it { should belong_to(:creator) }
     it { should have_many(:invitations).with_foreign_key('event_id') }
     it { should have_many(:event_attendee).with_foreign_key('event_id') }
     it { should have_many(:invitees).through(:invitations) }
-    
   end
 end
