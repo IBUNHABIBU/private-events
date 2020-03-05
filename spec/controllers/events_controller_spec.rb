df476dadf65b8ad10307bc737e47b70d96bdb7ad
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-05 17:34:09 +0000

    removing lint errors

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -9,17 +9,14 @@ RSpec.describe EventsController, type: :controller do
   end
   context 'GET #show' do
     it 'returns a success response' do
-      event = create(:event)
       get :show
       expect(response).to be_success
     end
     it 'renders the #show view' do
-      event = create(:event)
       get :show, params: { id: user.to_param }
       response.should render_template :show
     end
     it 'assigns the requested event to @event' do
-      user = create(:user)
       get :show, params: { id: user.to_param }
       assigns(:event).should eq(event)
     end
@@ -27,7 +24,6 @@ RSpec.describe EventsController, type: :controller do
 
   describe 'GET #index' do
     it 'displays all events' do
-      event = create(:event)
       get :index
       expect(assigns(:events)).to eq([event])
     end
